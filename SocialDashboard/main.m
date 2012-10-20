76dae776bad7fc311bdd1d6f2f5ca4ebee81013b
aty083 <alan.tl.yeung@gmail.com>
2012-10-20 13:43:53 -0600

    initial project

---
+++ SocialDashboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocialDashboard
+//
+//  Created by Alan Yeung on 2012-10-20.
+//  Copyright (c) 2012 Apps4Good. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
