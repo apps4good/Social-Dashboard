d4e7fab0aa9053bb07dba01706a41830c7a6d630
Dan Merino <dmerino@vendasta.com>
2012-10-21 12:29:13 -0600

    Added Twtitter

--- SocialDashboard/Classes/A4GFeedTableViewController.m
+++ SocialDashboard/Classes/A4GFeedTableViewController.m
@@ -9,8 +9,13 @@
 #import "A4GFeedTableViewController.h"
 
 @interface A4GFeedTableViewController ()
-
+{
+    NSMutableArray *tweetData;
+    NSMutableArray *tweetUser;
+}
 @end
+
+
 
 @implementation A4GFeedTableViewController
 
@@ -32,6 +37,38 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    tweetData = [NSMutableArray new];
+    tweetUser = [NSMutableArray new];
+    
+    
+    
+    TWRequest *postRequest = [[TWRequest alloc] initWithURL:[NSURL URLWithString:@"http://search.twitter.com/search.json?q=%40danmerino"] parameters:nil requestMethod:TWRequestMethodGET];
+    
+
+	
+	// Perform the request created above and create a handler block to handle the response.
+	[postRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+		NSString *output;
+		
+		if ([urlResponse statusCode] == 200) {
+			// Parse the responseData, which we asked to be in JSON format for this request, into an NSDictionary using NSJSONSerialization.
+			NSError *jsonParsingError = nil;
+			NSDictionary *publicTimeline = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonParsingError];
+			output = [NSString stringWithFormat:@"HTTP response status: %i\nPublic timeline:\n%@", [urlResponse statusCode], publicTimeline];
+            NSArray *tweets = [publicTimeline objectForKey:@"results"];
+            for (NSDictionary *tweet in tweets) {
+                [tweetData addObject:[tweet objectForKey:@"text"]];
+                [tweetUser addObject:[tweet objectForKey:@"from_user"]];
+            }
+		}
+		else {
+			output = [NSString stringWithFormat:@"HTTP response status: %i\n", [urlResponse statusCode]];
+		}
+        [self.tableView reloadData];
+        
+	}];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -51,7 +88,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 20;
+    return tweetData.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -65,10 +102,14 @@
         cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: CellIdentifier];
     }
     
-    // Configure the cell...
-    cell.textLabel.text = [NSString stringWithFormat: @"Master title: %d", indexPath.row];
-    cell.detailTextLabel.text =  [NSString stringWithFormat: @"Detail title: %d", indexPath.row];
-//    cell.imageView.image = ; image
+        NSString *tweet = [tweetData objectAtIndex:indexPath.row];
+        NSString *user  = [tweetUser objectAtIndex:indexPath.row];
+        NSLog(@"Fetching%d", [tweetData count]);
+        cell.textLabel.text = [NSString stringWithFormat: @"@%@", user];
+        cell.detailTextLabel.lineBreakMode = UILineBreakModeWordWrap;
+        cell.detailTextLabel.numberOfLines = 0;
+        cell.detailTextLabel.text =  [NSString stringWithFormat: @"%@", tweet];
+
     
     return cell;
 }
@@ -76,7 +117,7 @@
 #pragma mark - Table view delegate
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 66.0;
+    return 80.0;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
